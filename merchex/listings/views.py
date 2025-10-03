0d930902a292ae90e21bc900302cb771b9781db6
Tojosoa-coder <tojosoamahefa@gmail.com>
2025-10-03 22:01:32 +0300

    crud complet

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from listings.models import Band, Listing
+from listings.forms import ContactUsForm, BandForm, ListingForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
 
 def index(request):
+    return render(request, "listings/index.html")
+
+
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, "listings/index.html", {"bands": bands})
+    return render(request, "listings/band_list.html", {"bands": bands})
+
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request, "listings/band_detail.html", {"band": band})
 
 
 def about(request):
@@ -17,5 +28,104 @@ def listing(request):
     return render(request, "listings/listes.html", {"listes": lists})
 
 
+def list_detail(request, id):
+    list = Listing.objects.get(id=id)
+    return render(request, "listings/list_detail.html", {"list": list})
+
+
 def contact(request):
-    return render(request, "listings/contact.html")
+    print("La méthode de requête est : ", request.method)
+    print("Les données POST sont : ", request.POST)
+
+    if request.method == "POST":
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data["email"],
+                recipient_list=["admin@merchex.xyz"],
+            )
+            return redirect("email_sent")
+    else:
+        form = ContactUsForm()
+    return render(request, "listings/contact.html", {"form": form})
+
+
+def email_sent(request):
+    return render(request, "listings/email_sent.html")
+
+
+def band_create(request):
+
+    if request.method == "POST":
+        bandForm = BandForm(request.POST)
+        if bandForm.is_valid():
+            bandForm.save()
+            return redirect("band_list")
+    else:
+        bandForm = BandForm()
+    return render(request, "listings/band_create.html", {"form": bandForm})
+
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == "POST":
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect("band_detail", band.id)
+    else:
+        form = BandForm(instance=band)
+    return render(request, "listings/band_update.html", {"form": form, "band": band})
+
+
+def list_create(request):
+
+    if request.method == "POST":
+        listingForm = ListingForm(request.POST)
+        if listingForm.is_valid():
+            listingForm.save()
+            return redirect("listes")
+    else:
+        listingForm = ListingForm()
+    return render(request, "listings/list_create.html", {"form": listingForm})
+
+
+def list_update(request, id):
+
+    listing = Listing.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect("list_detail", listing.id)
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(
+        request, "listings/list_update.html", {"form": form, "listing": listing}
+    )
+
+
+def list_delete(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == "POST":
+        listing.delete()
+        return redirect("listes")
+
+    return render(request, "listings/list_delete.html", {"listing": listing})
+
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == "POST":
+        band.delete()
+        return redirect("band_list")
+
+    return render(request, "listings/band_delete.html", {"band": band})
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -14,14 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from listings import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.index),
-    path('about-us/', views.about),
-    path('contact-us/', views.contact),
-    path('listes/', views.listing),
+    path("admin/", admin.site.urls, name="admin"),
+    path("", views.index, name="index"),
+    path("bands/", views.band_list, name="band_list"),
+    path("bands/<int:id>/", views.band_detail, name="band_detail"),
+    path("bands/add/", views.band_create, name="band_create"),
+    path("bands/update/<int:id>", views.band_update, name="band_update"),
+    path("bands/delete/<int:id>", views.band_delete, name="band_delete"),
+    path("about-us/", views.about, name="about"),
+    path("contact-us/", views.contact, name="contact"),
+    path("listes/", views.listing, name="listes"),
+    path("listes/<int:id>/", views.list_detail, name="list_detail"),
+    path("listes/add/", views.list_create, name="list_create"),
+    path("listes/update/<int:id>", views.list_update, name="list_update"),
+    path("listes/delete/<int:id>", views.list_delete, name="list_delete"),
+    path("email-sent/", views.email_sent, name="email_sent"),
 ]
